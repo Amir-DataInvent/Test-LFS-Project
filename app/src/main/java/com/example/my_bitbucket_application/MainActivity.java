0ff099fb8c1f4ff5d26967239fec7dcb16db2bed
Amir-DataInvent <amir.sohail@datainvent.com>
2022-03-24 17:17:31 +0500

    added a second large file

--- app/src/main/java/com/example/my_bitbucket_application/MainActivity.java
+++ app/src/main/java/com/example/my_bitbucket_application/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //added a large file of 113Mb
+        //added a second large file of 113Mb
     }
 }
